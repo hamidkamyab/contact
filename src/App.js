74405a6bbea3d1ae427b8a6c7e215a9c13bd20ec
Hamid Kamyab <mr_kamyab@yahoo.com>
2024-04-26 23:52:05 +0330

    Complete the contact form project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import { useEffect, useState } from "react";
 import * as Vsc from "react-icons/vsc";
+import emailjs from '@emailjs/browser'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useState } from "react";
 
 function App() {
+
+  const [loading,setLoading] = useState(false);
+
+  const sendEmail = (e) => {
+      e.preventDefault();
+      setLoading(true);
+      emailjs.sendForm('service_x3fysx9','template_ng8pzz8',e.target,{
+        publicKey: 'Cfm89S9r4DYcYVxGo',
+      })
+      .then((res)=>{
+        if(res.status == 200){
+          toast("پیام با موفقیت ارسال شد!");
+          setLoading(false);
+        }
+      },(error)=>{
+        toast.error("ارسال پیام با شکست مواجه شد!");
+        setLoading(false);
+      })
+      
+  }
 
   const handleFocus = (e) => {
     let tags = document.querySelectorAll('.form-group');
@@ -16,10 +39,10 @@ function App() {
   return (
     <div className="App vh-100 d-flex align-items-center justify-content-center position-relative">
       <div className="main row flex-grow-1 m-0 p-5 p-md-0 d-flex justify-content-center">
-
+        <ToastContainer />
         <div className="formBox col-12 col-sm-8 col-md-5 col-lg-4 col-xl-3 bg-white p-4">
           <h6 className="text-muted text-center mb-3 mt-1 p-0 position-relative">ارسال پیام به مدیریت</h6>
-          <form className="d-flex gap-4 flex-column">
+          <form className="d-flex gap-4 flex-column" onSubmit={(e)=>sendEmail(e)}>
             <div className="form-group position-relative">
               <input type="text" name="name" className="form-control" onFocus={(e) => handleFocus(e)} placeholder="نام شما..." required />
             </div>
@@ -30,12 +53,17 @@ function App() {
               <input type="text" name="title" className="form-control" onFocus={(e) => handleFocus(e)} placeholder="عنوان پیام شما..." required />
             </div>
             <div className="form-group position-relative">
-              <textarea name="body" className="form-control" rows="4" onFocus={(e) => handleFocus(e)} placeholder="متن پیام شما..." required></textarea>
+              <textarea name="message" className="form-control" rows="4" onFocus={(e) => handleFocus(e)} placeholder="متن پیام شما..." required></textarea>
             </div>
 
             <div className="w-100">
-              <button className="btn btn-primary w-100">
-                ارســال پیــام
+              <button className="sendBtn btn btn-primary w-100 d-flex align-items-center justify-content-center gap-2" disabled={loading} >
+              {
+                loading &&
+                <span class="spinner-grow spinner-grow-sm" aria-hidden="true"></span>
+                
+              }
+              <span>ارســال پیــام</span>
               </button>
             </div>
           </form>
